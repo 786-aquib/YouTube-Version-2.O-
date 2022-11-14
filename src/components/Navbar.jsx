e61f7b96ede4f4cabf68d84eda58bf1dd5e22be3
Aquib <aquibmahmood106@gmail.com>
2022-11-14 19:10:49 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
-import { SearchBar } from ".";
 
 import { logo } from "../utils/constants";
+import { SearchBar } from "./";
 
 const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
